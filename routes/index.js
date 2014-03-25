8d41aa2b7c4a7aac8245702f16bdb0054483cd32
hammadmlk <hammadmlk@gmail.com>
2014-03-25 00:11:09 -0400

    redirect to index.html

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  
+  res.writeHead(302, {
+    'Location': '/index.html'
+    });
+
+  
+  
+  
+  
 };
